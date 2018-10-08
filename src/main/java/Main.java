77aa8257fa7d38bd8faee4d153483d9462139adb
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-10-08 18:07:49 +0300

    Generated files for Go Parser.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import antlr.*;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class Main {
     public static void main(String[] args) {
-
+        GoLexer lexer = new GoLexer(CharStreams.fromString("hello world"));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        GoParser parser = new GoParser(tokens);
     }
 }
