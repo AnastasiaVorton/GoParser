cc88cc614c438c078b957ec1aeb3a232f97add91
Bulat Khabirov <khabiroff_b@me.com>
2018-10-12 18:34:53 +0300

    JSON representation 50% done, regenerated parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,55 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        GoLexer lexer = new GoLexer(CharStreams.fromString("hello world"));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        GoParser parser = new GoParser(tokens);
-        ParseTree tree = parser.sourceFile();
+    private static String toJson(ParseTree tree) {
+        Gson prettyPrint = new GsonBuilder().setPrettyPrinting().create();
+        return prettyPrint.toJson(toMap(tree));
+    }
 
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        String jsonOutput = gson.toJson(parser);
-//        System.out.println(jsonOutput);
+    private static Map<String, Object> toMap(ParseTree tree) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        traverse(tree, map);
+        return map;
+    }
+
+    private static void traverse(ParseTree tree, Map<String, Object> map) {
+
+        if (tree instanceof TerminalNodeImpl) {
+            Token token = ((TerminalNodeImpl) tree).getSymbol();
+
+            map.put("type", GoParser.VOCABULARY.getSymbolicName(token.getType()));
+            map.put("text", token.getText());
+        }
+        else {
+            List<Map<String, Object>> children = new ArrayList<>();
+            String name = tree.getClass().getSimpleName().replaceAll("Context$", "");
+            map.put(Character.toLowerCase(name.charAt(0)) + name.substring(1), children);
+
+            for (int i = 0; i < tree.getChildCount(); i++) {
+                Map<String, Object> nested = new LinkedHashMap<>();
+                children.add(nested);
+                traverse(tree.getChild(i), nested);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            GoLexer lexer = new GoLexer(CharStreams.fromFileName("in.txt"));
+            GoParser parser = new GoParser(new CommonTokenStream(lexer));
+            System.out.println(toJson(parser.sourceFile()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
