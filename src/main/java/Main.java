e445b4cd0b6ec6eec0d7853eacea38ab0f00a2e0
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-10-12 15:06:46 +0300

    Add GSON imports ans trials to implement conversion to json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 import antlr.*;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         GoLexer lexer = new GoLexer(CharStreams.fromString("hello world"));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         GoParser parser = new GoParser(tokens);
+        ParseTree tree = parser.sourceFile();
+
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        String jsonOutput = gson.toJson(parser);
+//        System.out.println(jsonOutput);
     }
 }
