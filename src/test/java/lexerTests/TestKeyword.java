4d99dd53f705d47fe090df221ea6461465d27f1c
Bulat Khabirov <khabiroff_b@me.com>
2018-10-14 17:04:45 +0300

    Refactored TestKeyword

--- src/test/java/lexerTests/TestKeyword.java
+++ src/test/java/lexerTests/TestKeyword.java
@@ -1,28 +1,59 @@
 package lexerTests;
 
 import antlr.GoLexer;
-import antlr.GoParser;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static shared.Assert.assertToken;
 
 public class TestKeyword {
 
-    private static void assertKeyword(String expr) throws AssertionError {
-        Token token = Utils.getTokens(expr).get(0);
-
-    }
-
     @Test
-    public void breakKeyword() {
-        assertKeyword("break");
+    public void allKeywords() {
+        String allKeywords = "break "
+                + "default "
+                + "func "
+                + "interface "
+                + "select "
+                + "case "
+                + "defer "
+                + "go "
+                + "map "
+                + "struct "
+                + "chan "
+                + "else "
+                + "goto "
+                + "package "
+                + "switch "
+                + "fallthrough "
+                + "const "
+                + "if "
+                + "range "
+                + "type "
+                + "continue "
+                + "for "
+                + "import "
+                + "return "
+                + "var";
+        
+        String[] lexemes = allKeywords.split(" ");
+        List<? extends Token> tokens = Utils.getTokens(allKeywords).stream()
+                .filter(t -> !t.getText().equals(" ")).collect(Collectors.toList());
+
+        assertEquals(lexemes.length, tokens.size());
+
+        for (int i = 0; i < lexemes.length; i++) {
+            String lexeme = lexemes[i];
+            Token token = tokens.get(i);
+
+            // only keywords have their types translated to their exact names, but surrounded with '
+            assertEquals("'" + lexeme + "'", GoLexer.VOCABULARY.getDisplayName(token.getType()));
+            assertEquals(lexeme, token.getText());
+        }
     }
 
     @Test
@@ -33,6 +64,7 @@ public class TestKeyword {
 
         Token first = tokens.get(0);
 
+        // expect the token to be matched as an identifier
         assertToken(first, GoLexer.IDENTIFIER, expr.substring(0, 4));
     }
 
